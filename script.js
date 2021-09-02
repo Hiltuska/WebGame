c2a8b0f3a63029574bba122deedcd58ca45f846d
Hiltuska <47691185+Hiltuska@users.noreply.github.com>
2021-09-02 12:44:10 +0300

    Added bit of character variables

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var character
+var character = document.getElementById("character");
+
+function character(){
+  character.width = width;
+  character.height = height;
+  character.speedX = 0;
+  character.speedY = 0;
+  character.x = x;
+  character.y = y;
+}
